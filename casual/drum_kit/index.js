52e0de640cc30e5f7191cba4d6ddf76853214846
Ahmed Khaled <ahmed.khaled.hussain@gmail.com>
2023-05-03 00:14:02 +0300

    Update drum-kit solution

--- casual/drum_kit/index.js
+++ casual/drum_kit/index.js
@@ -1,85 +1,53 @@
-// Detecting Button Press
+"use strict";
 
-var numOfDrumButtons = document.querySelectorAll(".drum").length
+const drums = document.querySelectorAll(".drum");
 
-for (var i = 0; i < numOfDrumButtons; i++) {
-    
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-         
-        var buttonInnerHTML = this.innerHTML;
-        
-        makeSound(buttonInnerHTML);
-		
-		buttonAnimation(buttonInnerHTML);
-        
-    });
-    
-}
-
-// Detecting Keyboard Press
-
-document.addEventListener("keydown", function(event) {
-    
-    makeSound(event.key);
-	
-	buttonAnimation(event.key);
-    
-});
-
-// Make Sound
-
-function makeSound(key) {
-    
-    switch (key) {
-            case "w":
-                    var tom1 = new Audio("sounds/tom-1.mp3");
-                    tom1.play();
-                break;
-            
-            case "a":
-                    var tom2 = new Audio("sounds/tom-2.mp3");
-                    tom2.play();
-                break;
-                
-            case "s":
-                    var tom3 = new Audio("sounds/tom-3.mp3");
-                    tom3.play();
-                break;
-                
-            case "d":
-                    var tom4 = new Audio("sounds/tom-4.mp3");
-                    tom4.play();
-                break;
-                
-            case "j":
-                    var snare = new Audio("sounds/snare.mp3");
-                    snare.play();
-                break;
-                
-            case "k":
-                    var crash = new Audio("sounds/crash.mp3");
-                    crash.play();
-                break;
-                
-            case "l":
-                    var kick_bass = new Audio("sounds/kick-bass.mp3");
-                    kick_bass.play();
-                break;
-                
-            default: console.log(buttonInnerHTML)
-                
+drums.forEach(drum => {
+    drum.addEventListener("click", () => {
+        updateUI(drum)
+        makeSound(drum.innerHTML)
+    }
+    )
+    document.addEventListener("keydown", (event) => {
+        if (event.key === drum.innerHTML) {
+            updateUI(drum)
+            makeSound(drum.innerHTML)
         }
+    }
+    )
+})
+
+function updateUI(drumReference) {
+    drumReference.classList.add("pressed")
+    setTimeout(() => {
+        drumReference.classList.remove("pressed")
+    }, 100)
 }
 
-
-function buttonAnimation(currentKey) {
-	
-	var activeButton = document.querySelector("." + currentKey);
-	
-	activeButton.classList.add("pressed");
-	
-	setTimeout(function() {
-		activeButton.classList.remove("pressed");
-	}, 100)
-	
+function makeSound(letter) {
+    let letterSound = new Audio();
+    switch (letter) {
+        case 'w':
+            letterSound.src = "sounds/tom-1.mp3"
+            break;
+        case 'a':
+            letterSound.src = "sounds/tom-2.mp3"
+            break;
+        case 's':
+            letterSound.src = "sounds/tom-3.mp3"
+            break;
+        case 'd':
+            letterSound.src = "sounds/tom-4.mp3"
+            break;
+        case 'j':
+            letterSound.src = "sounds/crash.mp3"
+            break;
+        case 'k':
+            letterSound.src = "sounds/kick-bass.mp3"
+            break;
+        case 'l':
+            letterSound.src = "sounds/snare.mp3"
+            break;
+    }
+    letterSound.play();
 }
